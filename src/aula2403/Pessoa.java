8996c5a45222997e491363d34578af440f052660
DOUGLAS LEAL GONCALVES DE QUEIROZ <douglas.queiroz000@cs.unicid.edu.br>
2026-03-24 19:48:12 -0300

    aula2403

---
+++ src/aula2403/Pessoa.java
@@ -0,0 +1,11 @@
+package aula2403;
+
+public class Pessoa {
+
+	//atributos da classe >> caracteristicas
+	int idade;
+	String nome;
+
+
+}
+
---
+++ src/aula2403/Principal.java
@@ -0,0 +1,16 @@
+package aula2403;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// chamo uma classe >> instanciar a classe
+		Pessoa p = new Pessoa();
+		int a = 10;
+	
+	
+		System.out.println("Nome.......:" + p.nome);
+		System.out.println("Idade.......:" + p.idade);
+	}
+	
+
+}
